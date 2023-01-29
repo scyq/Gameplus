898f573d14cf9eb9b300d06758a28ce1eea5ef17
SCYQ\Administrator <chenyuqing0429@foxmail.com>
2023-01-29 17:41:14 +0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Math
+const Rad2Deg = 180 / Math.PI;
+const Deg2Rad = Math.PI / 180;
+
 // General Parameters
 let backgroundColor = null;
 let canvasWidth = 640;
@@ -5,22 +9,40 @@ let canvasHeight = 640;
 
 // Game Control
 let mode = "instructions";
+let outerSculptureMode = true;
+let gameStage = 0;
+let gameStages = ["雕刻", "后处理"];
 
 // instruction
 let showInstructions = true;
 
 // Color
 const palette = {
-    faceColor: []
+	faceColor: [],
 };
+let colors = null;
+let colors1 = "ed1c24-fdfffc-235789-f1d302-020100".split("-").map((a) => "#" + a);
+let colors2 = "2b2d42-8d99ae-edf2f4-ef233c-d80032".split("-").map((a) => "#" + a);
+let colors3 = "d6fff6-231651-4dccbd-2374ab-ff8484".split("-").map((a) => "#" + a);
+let colors4 = "a9e5bb-fcf6b1-f7b32b-f72c25-2d1e2f".split("-").map((a) => "#" + a);
+let colors5 = "ff8811-f4d06f-fff8f0-9dd9d2-392f5a".split("-").map((a) => "#" + a);
+let colors6 = "c5d86d-261c15-f7f7f2-e4e6c3-f05d23".split("-").map((a) => "#" + a);
+let colors7 = "ff6700-ebebeb-c0c0c0-3a6ea5-004e98".split("-").map((a) => "#" + a);
+let colors8 = "020202-0d2818-04471c-058c42-16db65".split("-").map((a) => "#" + a);
+let colors9 = "003049-d62828-f77f00-fcbf49-eae2b7".split("-").map((a) => "#" + a);
+let colors10 = "00072d-001c55-0a2472-0e6ba8-a6e1fa".split("-").map((a) => "#" + a);
+let colors11 = "6b2d5c-f0386b-ff5376-f8c0c8-e2c290".split("-").map((a) => "#" + a);
+let colors12 = "fb8b24-d90368-820263-291720-04a777".split("-").map((a) => "#" + a);
 
 // Changeable Parameters
 const Params = {
-    eyesRadius: null,
-    sculptureForce: 10,
-    sculptureRadius: 50,
-    faceNoise: true,
-}
+	eyesRadius: null,
+	sculptureForce: 10,
+	sculptureRadius: 50,
+	scale: 1.0,
+	rotation: 0.0, // Degree
+	faceNoise: true,
+};
 
 // Noise Parameters
 let xoff = 0;
@@ -49,276 +71,395 @@ let mouthAnchorPosition = [];
 let mousePressedTime = 0;
 let continuesSculptThreshold = 1000;
 
-function drawGradientBackground() {
-    let circleRadius = 200;
-    let baseMaxAlpha = 100;
+// Parameters Panel
+let gui = null;
 
-    push();
-    for (let i = 0; i < width; i++) {
-        for (let j = 0; j < height; j++) {
-            let distanceBetweenCenter = dist(i, j, width / 2, height / 2);
-            // 绘制中心圆
-            if (distanceBetweenCenter < circleRadius) {
-                let alpha =
-                    map(j, 0, height, baseMaxAlpha, 0);
-                let pixelColor = color(
-                    red(backgroundColor),
-                    green(backgroundColor),
-                    blue(backgroundColor),
-                    alpha
-                );
-                fill(pixelColor);
-                noStroke();
-                ellipse(i, j, 1, 1);
-            }
-            // 绘制背景
-            else {
-                let alpha = map(j, 0, height, 0, baseMaxAlpha);
-                let pixelColor = color(
-                    red(backgroundColor),
-                    green(backgroundColor),
-                    blue(backgroundColor),
-                    alpha
-                );
-                fill(pixelColor);
-                noStroke();
-                ellipse(i, j, 1, 1);
-            }
-        }
-    }
-    pop();
-}
+// Background
+let backgroundType;
+let backgroundFunction = null;
 
 function grainFilter() {
-    loadPixels();
-    for (let i = 3; i < pixels.length; i += 4) {
-        pixels[i] += randomGaussian(0, 20);
-    }
-    updatePixels();
+	loadPixels();
+	for (let i = 3; i < pixels.length; i += 4) {
+		pixels[i] += randomGaussian(0, 20);
+	}
+	updatePixels();
+}
+
+function checkMouseInCanvas() {
+	return mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height;
+}
+
+function randomBackgroundParams() {
+	backgroundColor = color(random(255), random(255), random(255));
+	backgroundType = random(["rect", "circle", "triangle", "strip", "wave"]);
+
+	colors = random([
+		colors1,
+		colors2,
+		colors3,
+		colors4,
+		colors5,
+		colors6,
+		colors7,
+		colors8,
+		colors9,
+		colors10,
+		colors11,
+		colors12,
+	]);
+	colors = colors.sort((a, b) => (random() < 0.5 ? 1 : -1));
 }
 
 function setup() {
-    createCanvas(canvasWidth, canvasHeight);
+	createCanvas(canvasWidth, canvasHeight);
 
-    // 初始化种子
-    let seed = new Date().getTime();
-    randomSeed(seed);
-    noiseSeed(seed);
+	// 初始化种子
+	let seed = new Date().getTime();
+	randomSeed(seed);
+	noiseSeed(seed);
 
-    textAlign(CENTER);
-    backgroundColor = color(random(255), random(255), random(255));
+	textAlign(CENTER);
+	randomBackgroundParams();
 
-    // face
-    facePointCounts = floor(2 * PI / faceAngleStep);
-    palette.faceColor = [random(255), random(255), random(255), random(180, 220)];
-    faceRadius.length = facePointCounts;
-    faceRadius.fill(faceBaseRadius);
+	// face
+	facePointCounts = floor((2 * PI) / faceAngleStep);
+	palette.faceColor = [random(255), random(255), random(255), random(180, 220)];
+	faceRadius.length = facePointCounts;
+	faceRadius.fill(faceBaseRadius);
 
-    // eyes
-    halfEyesGap = random(25, 50);
-    eyesXAnchor = width / 2;
-    eyesYAnchor = height / 2 - 40;
-    Params.eyesRadius = random(5, 40);
-    eyesPositions = [
-        [eyesXAnchor - halfEyesGap + 10 * randomGaussian(0, 1), eyesYAnchor + 10 * randomGaussian(0, 1)],
-        [eyesXAnchor + halfEyesGap + 10 * randomGaussian(0, 1), eyesYAnchor + 10 * randomGaussian(0, 1)]
-    ]
+	// eyes
+	halfEyesGap = random(25, 50);
+	eyesXAnchor = width / 2;
+	eyesYAnchor = height / 2 - 40;
+	Params.eyesRadius = random(5, 40);
+	eyesPositions = [
+		[eyesXAnchor - halfEyesGap + 10 * randomGaussian(0, 1), eyesYAnchor + 10 * randomGaussian(0, 1)],
+		[eyesXAnchor + halfEyesGap + 10 * randomGaussian(0, 1), eyesYAnchor + 10 * randomGaussian(0, 1)],
+	];
 
-    // 找到眼睛中间高度
-    let eyesMiddleHeight = (eyesPositions[0][1] + eyesPositions[1][1]) / 2;
-    let gapBetweenEyesAndMouth = random(20, 100);
+	// 找到眼睛中间高度
+	let eyesMiddleHeight = (eyesPositions[0][1] + eyesPositions[1][1]) / 2;
+	let gapBetweenEyesAndMouth = random(20, 100);
 
-    mouthXAnchor = width / 2; // 嘴巴的最左侧
-    mouthYAnchor = eyesMiddleHeight + gapBetweenEyesAndMouth;
-    halfMouthWidth = random(20, 100);
+	mouthXAnchor = width / 2; // 嘴巴的最左侧
+	mouthYAnchor = eyesMiddleHeight + gapBetweenEyesAndMouth;
+	halfMouthWidth = random(20, 100);
 
-    // 基于贝塞尔曲线画嘴，先定义两个锚点
-    mouthAnchorPosition = [
-        [mouthXAnchor - halfMouthWidth + 10 * randomGaussian(0, 1), mouthYAnchor + randomGaussian(0, 1)],
-        [mouthXAnchor + halfMouthWidth + 10 * randomGaussian(0, 1), mouthYAnchor + randomGaussian(0, 1)]
-    ]
+	// 基于贝塞尔曲线画嘴，先定义两个锚点
+	mouthAnchorPosition = [
+		[mouthXAnchor - halfMouthWidth + 10 * randomGaussian(0, 1), mouthYAnchor + randomGaussian(0, 1)],
+		[mouthXAnchor + halfMouthWidth + 10 * randomGaussian(0, 1), mouthYAnchor + randomGaussian(0, 1)],
+	];
 
-    initGUI();
+	initGUI();
 }
 
 function draw() {
-    // drawGradientBackground();
-    background(255);
+	// drawGradientBackground();
+	background(255);
 
-    if (mode == "instructions") {
-        instructions();
-        return;
-    } else {
-        drawFace();
-        drawFacialFeatures();
+	if (mode == "instructions") {
+		instructions();
+		return;
+	} else {
+		backgroundFunction ? backgroundFunction() : null;
+		drawFace();
+		drawFacialFeatures();
 
-        // 是否持续雕刻
-        if (mouseIsPressed) {
-            mousePressedTime += deltaTime;
-            if (mousePressedTime > continuesSculptThreshold) {
-                sculpt(Params.sculptureForce / 60);
-            }
-        }
-    }
+		// 是否持续雕刻
+		if (checkMouseInCanvas() && mouseIsPressed) {
+			mousePressedTime += deltaTime;
+			if (mousePressedTime > continuesSculptThreshold) {
+				sculpt(Params.sculptureForce / 60);
+			}
+		}
+	}
 
-    // grainFilter();
+	// grainFilter();
 
-    xoff += 0.005;
-    yoff += 0.005;
-    zoff += 0.005;
+	xoff += 0.005;
+	yoff += 0.005;
+	zoff += 0.005;
 }
 
+function polygon(x, y, radius, npoints) {
+	let angle = TWO_PI / npoints;
+	beginShape();
+	for (let a = 0; a < TWO_PI; a += angle) {
+		let sx = x + cos(a) * radius;
+		let sy = y + sin(a) * radius;
+		vertex(sx, sy);
+	}
+	endShape(CLOSE);
+}
 
 function instructions() {
-    push();
+	push();
 
-    textSize(22);
-    fill(255, 0, 0);
-    let title = "Make a Monster";
-    text(title, width / 2, height / 2);
+	textSize(22);
+	fill(255, 0, 0);
+	let title = "Make a Monster";
+	text(title, width / 2, height / 2);
 
-    textSize(16);
-    fill(0, 0, 0);
-    let instructions = "Click to make a face";
-    text(instructions, width / 2, height / 2 + 30);
-    pop();
+	textSize(16);
+	fill(0, 0, 0);
+	let instructions = "Click to make a face";
+	text(instructions, width / 2, height / 2 + 30);
+
+	pop();
+}
+
+function generalTransform() {
+	translate(width / 2, height / 2);
+	scale(Params.scale);
+	rotate(Params.rotation * Deg2Rad);
+	translate(-width / 2, -height / 2);
 }
 
 function drawFacialFeatures() {
-    push();
-    fill(0);
+	push();
+	fill(0);
+	generalTransform();
 
-    // eyes
-    circle(eyesPositions[0][0], eyesPositions[0][1], Params.eyesRadius);
-    circle(eyesPositions[1][0], eyesPositions[1][1], Params.eyesRadius);
+	// eyes
+	circle(eyesPositions[0][0], eyesPositions[0][1], Params.eyesRadius);
+	circle(eyesPositions[1][0], eyesPositions[1][1], Params.eyesRadius);
 
-    // mouth
-    stroke(0);
-    noFill();
-    strokeWeight(2);
-    beginShape();
-    let mouthCenterX = (mouthAnchorPosition[0][0] + mouthAnchorPosition[1][0]) / 2;
-    let mouthCenterY = (mouthAnchorPosition[0][1] + mouthAnchorPosition[1][1]) / 2;
-    vertex(mouthAnchorPosition[0][0], mouthAnchorPosition[0][1]);    // 起始锚点
-    quadraticVertex(mouthCenterX + 20 * noise(xoff, yoff, zoff),
-        mouthCenterY + 20 * noise(xoff, yoff, zoff),
-        mouthAnchorPosition[1][0], mouthAnchorPosition[1][1]);    // 控制点和结束锚点
+	// mouth
+	stroke(0);
+	noFill();
+	strokeWeight(2);
+	beginShape();
+	let mouthCenterX = (mouthAnchorPosition[0][0] + mouthAnchorPosition[1][0]) / 2;
+	let mouthCenterY = (mouthAnchorPosition[0][1] + mouthAnchorPosition[1][1]) / 2;
+	vertex(mouthAnchorPosition[0][0], mouthAnchorPosition[0][1]); // 起始锚点
+	quadraticVertex(
+		mouthCenterX + 20 * noise(xoff, yoff, zoff),
+		mouthCenterY + 20 * noise(xoff, yoff, zoff),
+		mouthAnchorPosition[1][0],
+		mouthAnchorPosition[1][1]
+	); // 控制点和结束锚点
 
-    endShape();
-    noStroke();
+	endShape();
+	noStroke();
 
-    pop();
+	pop();
 }
 
 function drawFace() {
-    push();
-    noStroke();
-    fill(color(...palette.faceColor));
-    beginShape();
-    let xCenter = width / 2;
-    let yCenter = height / 2;
-    let tightness = 0.5;
+	push();
+	noStroke();
+	generalTransform();
+	fill(color(...palette.faceColor));
+	beginShape();
+	let xCenter = width / 2;
+	let yCenter = height / 2;
+	let tightness = 0.5;
 
-    for (let i = 0; i < facePointCounts; i++) {
-        let theta = i * faceAngleStep;
-        let r1, r2;
-        if (theta < PI / 2) {
-            r1 = cos(theta);
-            r2 = 1;
-        } else if (theta < PI) {
-            r1 = 0;
-            r2 = sin(theta);
-        } else if (theta < 3 * PI / 2) {
-            r1 = sin(theta);
-            r2 = 0;
-        } else {
-            r1 = 1;
-            r2 = cos(theta);
-        }
-        let faceNoise = Params.faceNoise ? faceRadius[i] * noise(tightness * r1, tightness * r2, xoff) : 0;
-        let r = faceRadius[i] + faceNoise;
-        let x = xCenter + r * cos(theta);
-        let y = yCenter + r * sin(theta);
-        curveVertex(x, y);
-    }
+	for (let i = 0; i < facePointCounts; i++) {
+		let theta = i * faceAngleStep;
+		let r1, r2;
+		if (theta < PI / 2) {
+			r1 = cos(theta);
+			r2 = 1;
+		} else if (theta < PI) {
+			r1 = 0;
+			r2 = sin(theta);
+		} else if (theta < (3 * PI) / 2) {
+			r1 = sin(theta);
+			r2 = 0;
+		} else {
+			r1 = 1;
+			r2 = cos(theta);
+		}
+		let faceNoise = Params.faceNoise ? faceRadius[i] * noise(tightness * r1, tightness * r2, xoff) : 0;
+		let r = faceRadius[i] + faceNoise;
+		let x = xCenter + r * cos(theta);
+		let y = yCenter + r * sin(theta);
+		curveVertex(x, y);
+	}
 
-    endShape(CLOSE);
-    pop();
+	endShape(CLOSE);
+	pop();
 }
 
 // 隆起函数, 0 outside [-1, 1], argmax = 0
 function bump(x) {
-    if (abs(x) < 1) {
-        return (sin(PI * (x + 0.5)) + 1) / 2;
-        // return (exp(-1 / (1 - x ** 2)));
-    } else {
-        return 0;
-    }
+	if (abs(x) < 1) {
+		return (sin(PI * (x + 0.5)) + 1) / 2;
+		// return (exp(-1 / (1 - x ** 2)));
+	} else {
+		return 0;
+	}
 }
 
 function sculpt(force) {
-    let v = createVector(mouseX - width / 2, mouseY - height / 2);
-    let theta = map(v.heading(), -PI, PI, PI, 3 * PI);
-    let idx = floor(theta / faceAngleStep);
-    if (keyIsPressed) {
-        force *= -1;
-    }
+	let v = createVector(mouseX - width / 2, mouseY - height / 2);
+	let theta = map(v.heading(), -PI, PI, PI, 3 * PI);
+	let idx = floor(theta / faceAngleStep);
+	if (!outerSculptureMode) {
+		force *= -1;
+	}
 
-    let i1 = floor(idx - Params.sculptureRadius / 2);
-    let i2 = i1;
+	let i1 = floor(idx - Params.sculptureRadius / 2);
+	let i2 = i1;
 
-    if (i1 < 0) {
-        i1 += facePointCounts;
-    }
+	if (i1 < 0) {
+		i1 += facePointCounts;
+	}
 
-    while (i2 < idx + Params.sculptureRadius / 2) {
-        let x = (2 * (i2 - idx)) / Params.sculptureRadius;
-        let y = force * bump(x);
-        if (faceRadius[i1] + y > 0) {
-            faceRadius[i1] += y;
-        }
-        i1++;
-        if (i1 >= facePointCounts) {
-            i1 -= facePointCounts;
-        }
-        i2++;
-    }
+	while (i2 < idx + Params.sculptureRadius / 2) {
+		let x = (2 * (i2 - idx)) / Params.sculptureRadius;
+		let y = force * bump(x);
+		if (faceRadius[i1] + y > 0) {
+			faceRadius[i1] += y;
+		}
+		i1++;
+		if (i1 >= facePointCounts) {
+			i1 -= facePointCounts;
+		}
+		i2++;
+	}
 }
 
 function initGUI() {
-    const gui = new dat.GUI();
+	gui = new dat.GUI();
 
-    let noiseFolder = gui.addFolder("Noise");
-    noiseFolder.add(Params, "faceNoise").name("Face Noise");
-    noiseFolder.open();
+	let generalFolder = gui.addFolder("General");
+	generalFolder.add(Params, "scale", 0.1, 2.0).name("Scale");
+	generalFolder.add(Params, "rotation", -180, 180).name("Rotation");
+	generalFolder.open();
 
-    let faceFolder = gui.addFolder("Face");
-    faceFolder.addColor(palette, 'faceColor').name("Face Color");
-    faceFolder.add(Params, 'eyesRadius', 5, 40).name("Eyes Radius");
-    faceFolder.open();
+	let noiseFolder = gui.addFolder("Noise");
+	noiseFolder.add(Params, "faceNoise").name("Face Noise");
+	noiseFolder.open();
 
-    let sculptureFolder = gui.addFolder("Sculpture");
-    sculptureFolder.add(Params, 'sculptureForce', 1, 20).name("Force");
-    sculptureFolder.add(Params, 'sculptureRadius', 10, 100).name("Radius");
-    sculptureFolder.open();
+	let faceFolder = gui.addFolder("Face");
+	faceFolder.addColor(palette, "faceColor").name("Face Color");
+	faceFolder.add(Params, "eyesRadius", 5, 40).name("Eyes Radius");
+	faceFolder.open();
 
+	let sculptureFolder = gui.addFolder("Sculpture");
+	sculptureFolder.add(Params, "sculptureForce", 1, 20).name("Force");
+	sculptureFolder.add(Params, "sculptureRadius", 10, 100).name("Radius");
+	sculptureFolder.open();
+
+	gui.close();
 }
 
+function generateRandomGeometryBackground() {
+	push();
+
+	push();
+	//bg
+	fill(colors[3]);
+	rect(0, 0, width, height);
+	noStroke();
+	pop();
+
+	noStroke();
+	let cir = 200;
+	for (let y = -height / 2 - cir / 2; y < height / 2 + cir * 2; y += cir) {
+		let xSpan = cir;
+		if (backgroundType == "wave") {
+			beginShape();
+			y += cir;
+			xSpan = 40;
+		}
+		for (let x = -width / 2 - cir / 2; x < width / 2 + cir * 2; x += xSpan) {
+			fill(colors[4]);
+			if (backgroundType == "rect") {
+				rect(x, y, cir * 0.8, cir * 0.8);
+			} else if (backgroundType == "strip") {
+				rect(x, y, cir * 3, cir * 0.4);
+			} else if (backgroundType == "triangle") {
+				polygon(x, y, cir * 0.8, 3);
+			} else if (backgroundType == "wave") {
+				curveVertex(x, y + (sin(x / 100 + y + frameCount / (100 + sin(y / 50) * 50)) * cir) / 2);
+			} else {
+				ellipse(x, y, cir * 0.8, cir * 0.8);
+			}
+		}
+		if (backgroundType == "wave") {
+			stroke(colors[4]);
+			noFill();
+			strokeWeight(cir * 0.8);
+			endShape();
+		}
+	}
+	pop();
+}
+
+function generateColorBackground() {
+	push();
+	//bg
+	background(backgroundColor);
+	pop();
+}
+
+function generateGradientBackground() {
+	let circleRadius = 200;
+	let baseMaxAlpha = 100;
+
+	push();
+	for (let i = 0; i < width; i++) {
+		for (let j = 0; j < height; j++) {
+			let distanceBetweenCenter = dist(i, j, width / 2, height / 2);
+			// 绘制中心圆
+			if (distanceBetweenCenter < circleRadius) {
+				let alpha = map(j, 0, height, baseMaxAlpha, 0);
+				let pixelColor = color(red(backgroundColor), green(backgroundColor), blue(backgroundColor), alpha);
+				fill(pixelColor);
+				noStroke();
+				ellipse(i, j, 1, 1);
+			}
+			// 绘制背景
+			else {
+				let alpha = map(j, 0, height, 0, baseMaxAlpha);
+				let pixelColor = color(red(backgroundColor), green(backgroundColor), blue(backgroundColor), alpha);
+				fill(pixelColor);
+				noStroke();
+				ellipse(i, j, 1, 1);
+			}
+		}
+	}
+	pop();
+}
 
 function mouseClicked() {
+	if (checkMouseInCanvas()) {
+		if (mode == "instructions") {
+			mode = "play";
+			gui.open();
+			return;
+		}
 
-    if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
-        if (mode == "instructions") {
-            mode = "play";
-            return;
-        }
-
-        sculpt(Params.sculptureForce);
-    }
+		sculpt(Params.sculptureForce);
+	}
 }
 
 function keyPressed() {
-    if (key == "s") {
-        saveCanvas("MyMonster", "png");
-    }
+	if (key == "s") {
+		saveCanvas("MyMonster", "png");
+	}
+
+	if (key == "q") {
+		outerSculptureMode = !outerSculptureMode;
+		document.getElementById("sculpt-mode").innerHTML = `当前模式: <span class="attention">${
+			outerSculptureMode ? "外凸" : "内凹"
+		} </span>`;
+	}
+
+	if (key == "n") {
+		gameStage = (gameStage + 1) % gameStages.length;
+		document.getElementById(
+			"stage"
+		).innerHTML = `当前阶段: <span class="attention"> ${gameStages[gameStage]} </span>`;
+	}
+
+	if (key == "b") {
+		backgroundFunction = random([generateColorBackground, generateRandomGeometryBackground]);
+		randomBackgroundParams();
+	}
 }
